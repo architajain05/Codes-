0b3da2338f3c6269b91d3ad3a636b12313f7f356
Archita <122664337+architajain05@users.noreply.github.com>
2023-02-02 22:43:17 +0530

    Create to calculate power(x,n).c

---
+++ to calculate power(x,n).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int i,n,num,result=1;
+	printf("Enter any number:");
+	scanf("%d",&num);
+	printf("\n Till which power to calculate:");      
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+{
+	result=result*num;
+}
+  printf("\n pow(%d, %d)=%d",num,n, result);
+	return 0;
+}*/
